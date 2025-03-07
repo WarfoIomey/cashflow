8d6f5ae8494e841814e28d23f526d9bdddfdb2da
Kirill Turochkin <kirill180104@mail.ru>
2025-03-07 18:02:02 +0800

    added authorization and registration

--- dss/dss/urls.py
+++ dss/dss/urls.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+handler404 = 'cashflow.views.page_not_found'
+handler500 = 'cashflow.views.failure_server'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('cashflow.urls', namespace='cashflow')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('cashflow:index'),
+        ),
+        name='registration',
+    ),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
