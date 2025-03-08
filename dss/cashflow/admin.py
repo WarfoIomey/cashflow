399e560a86a971fbe968bb4e952cc170fe0f8e9e
Kirill Turochkin <kirill180104@mail.ru>
2025-03-08 20:37:23 +0800

    added admin panel for status record subcategory

--- dss/cashflow/admin.py
+++ dss/cashflow/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
-from .models import Category, Type
+from .models import Category, Record, Type, Status, Subcategory
 
 
 admin.site.register(Category)
 admin.site.register(Type)
+admin.site.register(Status)
+admin.site.register(Record)
+admin.site.register(Subcategory)
