c2d88c8dc0c065e077820f342a6d9de5be9b39ae
Kirill Turochkin <kirill180104@mail.ru>
2025-03-08 20:39:03 +0800

    added docstring

--- dss/cashflow/utils.py
+++ dss/cashflow/utils.py
@@ -1,25 +1,28 @@
 from django.shortcuts import get_object_or_404
 
-from .models import Type, Category, Subcategory, Status
+from .models import Category, Type, Subcategory, Status
 
 
 def get_types(user):
-    """Получение постов"""
+    """Получение записей."""
     return Type.objects.select_related('user').filter(user=user)
 
 
 def get_status(user):
+    """Получение статусов."""
     return Status.objects.select_related('user').filter(user=user)
 
 
 def get_categorys(pk, user):
-    return Category.objects.select_related('type','user').filter(
+    """Получение категорий."""
+    return Category.objects.select_related('type', 'user').filter(
         type=pk,
         user=user
     )
 
 
 def get_subcategorys(slug, user):
+    """Получение подкатегорий."""
     category = get_object_or_404(Category, slug=slug)
     return Subcategory.objects.select_related('category', 'user').filter(
         category=category.pk,
