3f35e7f168e105bb3ed24d8929f8adb96602b1b4
Kirill Turochkin <kirill180104@mail.ru>
2025-03-08 16:40:53 +0800

    change model Type

--- dss/cashflow/admin.py
+++ dss/cashflow/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Category, TypeMoney
+from .models import Category, Type
 
 
 admin.site.register(Category)
-admin.site.register(TypeMoney)
+admin.site.register(Type)
