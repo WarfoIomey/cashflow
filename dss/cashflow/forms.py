7304128b15129d982ef0c690add996f246e97f4b
Kirill Turochkin <kirill180104@mail.ru>
2025-03-08 16:41:41 +0800

    added form filter and improved form created record

--- dss/cashflow/forms.py
+++ dss/cashflow/forms.py
@@ -1,21 +1,55 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
-from .models import Record, TypeMoney, Category, SubCategory, StatusMoney
+from .models import Record, Type, Category, Subcategory, Status
 
 User = get_user_model()
 
 
+class RecordFilterForm(forms.Form):
+    start_date = forms.DateField(
+        required=False,
+        widget=forms.DateInput(attrs={"type": "date"}),
+        label="Дата с"
+    )
+    end_date = forms.DateField(
+        required=False,
+        widget=forms.DateInput(attrs={"type": "date"}),
+        label="Дата по"
+    )
+    status = forms.ModelChoiceField(
+        queryset=Status.objects.all(),
+        required=False,
+        empty_label="Все статусы"
+    )
+    type = forms.ModelChoiceField(
+        queryset=Type.objects.all(),
+        required=False,
+        empty_label="Все типы"
+    )
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        required=False,
+        empty_label="Все категории"
+    )
+    subcategory = forms.ModelChoiceField(
+        queryset=Subcategory.objects.all(),
+        required=False,
+        empty_label="Все подкатегории"
+    )
+
+
 class RecordForm(forms.ModelForm):
     status = forms.ModelChoiceField(
-        queryset=StatusMoney.objects.all(),
+        required=False,
+        queryset=Status.objects.all(),
         empty_label='Выберите статус',
         widget=forms.Select(attrs={
             'class': 'form-control',
         })
     )
     type = forms.ModelChoiceField(
-        queryset=TypeMoney.objects.all(),
+        queryset=Type.objects.all(),
         empty_label='Выберите тип',
         widget=forms.Select(attrs={
             'class': 'form-control',
@@ -30,13 +64,39 @@ class RecordForm(forms.ModelForm):
         })
     )
     subcategory = forms.ModelChoiceField(
-        queryset=SubCategory.objects.none(),
+        queryset=Subcategory.objects.none(),
         empty_label='Сначала выберите тип',
         widget=forms.Select(attrs={
             'class': 'form-control',
             'id': 'id_subcategory'
         })
     )
+    amount = forms.IntegerField(
+        required=True,
+        min_value=0
+    )
+    comment = forms.Textarea()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'type' in self.data:
+            try:
+                type_id = int(self.data.get('type'))
+                self.fields['category'].queryset = Category.objects.filter(
+                    type_id=type_id
+                )
+            except (ValueError, TypeError):
+                pass
+        if 'category' in self.data:
+            try:
+                category_id = int(self.data.get('category'))
+                self.fields[
+                    'subcategory'
+                ].queryset = Subcategory.objects.filter(
+                    category_id=category_id
+                )
+            except (ValueError, TypeError):
+                pass
 
     class Meta:
         model = Record
@@ -52,14 +112,14 @@ class RecordForm(forms.ModelForm):
 class StatusForm(forms.ModelForm):
 
     class Meta:
-        model = StatusMoney
+        model = Status
         fields = ('title', 'description')
 
 
 class TypeForm(forms.ModelForm):
 
     class Meta:
-        model = TypeMoney
+        model = Type
         fields = ('title', 'description')
 
 
@@ -73,7 +133,7 @@ class CategoryForm(forms.ModelForm):
 class SubcategoryForm(forms.ModelForm):
 
     class Meta:
-        model = SubCategory
+        model = Subcategory
         fields = ('title', 'description', 'slug')
 
 
